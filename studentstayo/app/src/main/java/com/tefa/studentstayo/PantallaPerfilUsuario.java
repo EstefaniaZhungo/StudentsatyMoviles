06e3f90809481efea5dfe851cead6d96ec7ee866
Edissonly <fabianleon184@gmail.com>
2025-03-07 20:59:29 -0500

    usuario final

--- studentstayo/app/src/main/java/com/tefa/studentstayo/PantallaPerfilUsuario.java
+++ studentstayo/app/src/main/java/com/tefa/studentstayo/PantallaPerfilUsuario.java
@@ -9,228 +9,229 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
-import com.android.volley.Response;
+import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+import com.tefa.studentstayo.model.Persona;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+
 public class PantallaPerfilUsuario extends AppCompatActivity {
-    private String cedula;
+
+    private String cedula, imagenBase64;
     private Long idCliente;
-    private EditText txtTelefono, txtnombre, txtnombre2, txtapellido, txtapellido2, txtcontrasena;
-    private Button btnEditar;
     private int edad;
+    private EditText txtTelefono, txtNombre, txtNombre2, txtApellido, txtApellido2, txtContrasena;
+    private Button btnEditar;
+    private ImageView imgPerfil;
+    private RequestQueue requestQueue;
+    private Persona persona;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_perfil_usuario);
-        // Cargar datos iniciales (incluye foto de perfil)
-        getDatos(PantallaPrincipal.correoUsuario);
-        // Configurar el botón de actualización para modificar los datos del usuario/persona (excepto la foto)
-        actualizarDatos();
+
+        requestQueue = Volley.newRequestQueue(this);
+        inicializarUI();
+        cargarDatos(PantallaPrincipal.correoUsuario);
     }
 
-    public void actualizarDatos() {
-        btnEditar = findViewById(R.id.btnEditar);
-        txtnombre = findViewById(R.id.txtNombre);
-        txtnombre2 = findViewById(R.id.txtNombre2);
-        txtapellido = findViewById(R.id.txtApellido);
-        txtapellido2 = findViewById(R.id.txtApellido2);
+    private void inicializarUI() {
+        txtNombre = findViewById(R.id.txtNombre);
+        txtNombre2 = findViewById(R.id.txtNombre2);
+        txtApellido = findViewById(R.id.txtApellido);
+        txtApellido2 = findViewById(R.id.txtApellido2);
         txtTelefono = findViewById(R.id.txtTelefono);
-        txtcontrasena = findViewById(R.id.pasword);
+        txtContrasena = findViewById(R.id.pasword);
+        imgPerfil = findViewById(R.id.imgPerfil);
+        btnEditar = findViewById(R.id.btnEditar);
 
-        btnEditar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Actualiza solo la contraseña y el usuario en la tabla clientes
-                updateDatosUsuario(txtcontrasena.getText().toString(), PantallaPrincipal.correoUsuario, idCliente);
-                // Actualiza solo los datos personales (nombre, apellido, teléfono) en la tabla personas
-                updateDatosPersona(
-                        txtnombre.getText().toString(),
-                        txtnombre2.getText().toString(),
-                        txtapellido.getText().toString(),
-                        txtapellido2.getText().toString(),
-                        txtTelefono.getText().toString(),
-                        edad,
-                        cedula
-                );
-                salir(v);
-            }
-        });
+        btnEditar.setOnClickListener(v -> actualizarDatos());
     }
 
-    // Metodo para obtener los datos del cliente, incluyendo la foto de perfil
-    public void getDatos(String usuario) {
+    private void cargarDatos(String usuario) {
         String url = Environment.BASE_URL + "/clientes/usuario/" + usuario;
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        try {
-                            if (response.length() > 0) {
-                                JSONObject jsonObjectCliente = response.getJSONObject(0);
-                                cedula = jsonObjectCliente.getString("cedula_persona");
-                                idCliente = jsonObjectCliente.getLong("idCliente");
-                                String contrasena = jsonObjectCliente.getString("contrasena");
-                                String imagenBase64 = jsonObjectCliente.getString("foto");
-                                if (imagenBase64.startsWith("data:image")) {
-                                    imagenBase64 = imagenBase64.split(",")[1];
-                                }
-                                Bitmap imagenBitmap = decodeBase64ToBitmap(imagenBase64);
-                                ImageView imgPerfil = findViewById(R.id.imgPerfil);
-                                imgPerfil.setImageBitmap(imagenBitmap);
 
-                                txtcontrasena = findViewById(R.id.pasword);
-                                txtcontrasena.setText(contrasena);
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, response -> {
+            try {
+                if (response.length() > 0) {
+                    JSONObject jsonObjectCliente = response.getJSONObject(0);
+                    cedula = jsonObjectCliente.optString("cedula_persona");
+                    idCliente = jsonObjectCliente.optLong("idCliente");
+                    txtContrasena.setText(jsonObjectCliente.optString("contrasena"));
 
-                                getDatosByCedula(cedula);
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Cliente no encontrado", Toast.LENGTH_LONG).show();
-                            }
-                        } catch (JSONException j) {
-                            j.printStackTrace();
-                        }
+                    imagenBase64 = jsonObjectCliente.optString("foto", "");
+                    if (!imagenBase64.isEmpty()) {
+                        imgPerfil.setImageBitmap(decodeBase64ToBitmap(imagenBase64));
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("VolleyError", "Error en la solicitud", error);
+
+                    if (!cedula.isEmpty()) {
+                        cargarDatosPersona(cedula);
+                    }
+                } else {
+                    mostrarMensaje("Cliente no encontrado");
+                }
+            } catch (JSONException e) {
+                manejarError(e);
             }
-        }
-        );
-        Volley.newRequestQueue(this).add(jsonArrayRequest);
+        }, this::manejarVolleyError);
+
+        requestQueue.add(request);
     }
 
-    // Metodo para decodificar una cadena Base64 a Bitmap
-    public Bitmap decodeBase64ToBitmap(String base64String) {
+    private void cargarDatosPersona(String cedula) {
+        String url = Environment.BASE_URL + "/personas/" + cedula;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
+            persona = new Persona(
+                    cedula,
+                    response.optString("nombre", ""),
+                    response.optString("nombre2", ""),
+                    response.optString("apellido", ""),
+                    response.optString("apellido2", ""),
+                    response.optString("telefono", ""),
+                    response.optString("direccion", ""),
+                    response.optInt("edad", 0),
+                    response.optString("genero", ""),
+                    response.optString("id_canton", ""),
+                    response.optString("nombreContactoEmergencia", ""),
+                    response.optString("telefonoContactoEmergencia", ""),
+                    response.optString("parentescoContactoEmergencia", ""),
+                    response.optString("nombreContactoEmergencia2", ""),
+                    response.optString("telefonoContactoEmergencia2", ""),
+                    response.optString("parentescoContactoEmergencia2", "")
+
+            );
+
+            // Cargar los datos en los campos de texto
+            txtNombre.setText(persona.getNombre());
+            txtNombre2.setText(persona.getNombre2());
+            txtApellido.setText(persona.getApellido());
+            txtApellido2.setText(persona.getApellido2());
+            txtTelefono.setText(persona.getTelefono());
+
+        }, this::manejarVolleyError);
+
+        requestQueue.add(request);
+    }
+
+
+    private void actualizarDatos() {
+        if (idCliente != null && cedula != null) {
+            actualizarUsuario();
+            actualizarPersona();
+            // Refrescar la actividad después de actualizar los datos
+            new android.os.Handler().postDelayed(this::recreate, 1000);
+        } else {
+            mostrarMensaje("No se pudieron actualizar los datos");
+        }
+    }
+
+    private void actualizarUsuario() {
+        String url = Environment.BASE_URL + "/clientes/" + idCliente;
+        JSONObject requestBody = new JSONObject();
         try {
-            byte[] decodedString = Base64.decode(base64String, Base64.DEFAULT);
-            return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            requestBody.put("contrasena", txtContrasena.getText().toString());
+            requestBody.put("usuario", PantallaPrincipal.correoUsuario);
+            requestBody.put("foto", imagenBase64); // Enviar la misma imagen ya cargada
+        } catch (JSONException e) {
+            manejarError(e);
+            return;
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, requestBody,
+                response -> mostrarSnackbar("Datos actualizado correctamente"), this::manejarVolleyError);
+
+        requestQueue.add(request);
+    }
+
+    private void mostrarSnackbar(String mensaje) {
+        Snackbar.make(findViewById(android.R.id.content), mensaje, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void actualizarPersona() {
+        if (persona == null) return;
+        String url = Environment.BASE_URL + "/personas/" + cedula;
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("nombre", txtNombre.getText().toString().isEmpty() ? persona.getNombre() : txtNombre.getText().toString());
+            requestBody.put("nombre2", txtNombre2.getText().toString().isEmpty() ? persona.getNombre2() : txtNombre2.getText().toString());
+            requestBody.put("apellido", txtApellido.getText().toString().isEmpty() ? persona.getApellido() : txtApellido.getText().toString());
+            requestBody.put("apellido2", txtApellido2.getText().toString().isEmpty() ? persona.getApellido2() : txtApellido2.getText().toString());
+            requestBody.put("telefono", txtTelefono.getText().toString().isEmpty() ? persona.getTelefono() : txtTelefono.getText().toString());
+            requestBody.put("direccion", persona.getDireccion());
+            requestBody.put("edad", persona.getEdad());
+            requestBody.put("genero", persona.getGenero()); // Si no se edita, se mantiene
+            requestBody.put("id_canton", persona.getId_canton());
+            requestBody.put("nombreContactoEmergencia", persona.getNombreContactoEmergencia());
+            requestBody.put("telefonoContactoEmergencia", persona.getTelefonoContactoEmergencia());
+            requestBody.put("parentescoContactoEmergencia", persona.getParentescoContactoEmergencia());
+            requestBody.put("nombreContactoEmergencia2", persona.getNombreContactoEmergencia2());
+            requestBody.put("telefonoContactoEmergencia2", persona.getTelefonoContactoEmergencia2());
+            requestBody.put("parentescoContactoEmergencia2", persona.getParentescoContactoEmergencia2());
+        } catch (JSONException e) {
+            manejarError(e);
+            return;
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, requestBody,
+                response -> {
+                }, this::manejarVolleyError);
+
+        requestQueue.add(request);
+    }
+
+
+    private Bitmap decodeBase64ToBitmap(String base64String) {
+        try {
+            if (base64String.startsWith("data:image")) {
+                base64String = base64String.split(",")[1];
+            }
+            byte[] decodedBytes = Base64.decode(base64String, Base64.DEFAULT);
+            return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
         } catch (IllegalArgumentException e) {
             Log.e("Base64 Error", "Error al decodificar Base64", e);
             return null;
         }
     }
 
-    // Metodo para obtener los datos de la persona
-    public void getDatosByCedula(String cedula) {
-        String url = Environment.BASE_URL + "/personas/" + cedula;
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            if (response.length() > 0) {
-                                String nombre = response.getString("nombre");
-                                String nombre2 = response.getString("nombre2");
-                                String apellido = response.getString("apellido");
-                                String apellido2 = response.getString("apellido2");
-                                String telefono = response.getString("telefono");
-                                edad = response.getInt("edad");
-
-                                txtnombre = findViewById(R.id.txtNombre);
-                                txtnombre2 = findViewById(R.id.txtNombre2);
-                                txtapellido = findViewById(R.id.txtApellido);
-                                txtapellido2 = findViewById(R.id.txtApellido2);
-                                txtTelefono = findViewById(R.id.txtTelefono);
-
-                                txtnombre.setText(nombre);
-                                txtnombre2.setText(nombre2);
-                                txtapellido.setText(apellido);
-                                txtapellido2.setText(apellido2);
-                                txtTelefono.setText(telefono);
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Persona no encontrada", Toast.LENGTH_LONG).show();
-                            }
-                        } catch (JSONException j) {
-                            j.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("JSON ERROR", error.getMessage());
-            }
-        }
-        );
-        Volley.newRequestQueue(this).add(jsonObjectRequest);
+    private String encodeBitmapToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] byteArray = baos.toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
     }
 
-    // Metodo para actualizar los datos del cliente (usuario y contraseña)
-    public void updateDatosUsuario(String contrasena, String usuario, Long id) {
-        String url = Environment.BASE_URL + "/clientes/" + id;
-        JSONObject requestBodyUsuario = new JSONObject();
-        try {
-            requestBodyUsuario.put("contrasena", contrasena);
-            requestBodyUsuario.put("usuario", usuario);
-        } catch (JSONException j) {
-            j.printStackTrace();
-        }
-
-        JsonObjectRequest jsonObjectRequestUsuario = new JsonObjectRequest(Request.Method.PUT, url, requestBodyUsuario,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Toast.makeText(getApplicationContext(), "ACTUALIZACIÓN CORRECTA", Toast.LENGTH_LONG).show();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        }
-        );
-        Volley.newRequestQueue(this).add(jsonObjectRequestUsuario);
+    private void manejarError(Exception e) {
+        Log.e("Error", "Error en la aplicación", e);
     }
 
-    // Metodo para actualizar los datos de la persona (nombre, apellido y teléfono)
-    public void updateDatosPersona(String nom1, String nom2, String ape1, String ape2, String tel, int edad, String cedula) {
-        String url = Environment.BASE_URL + "/personas/" + cedula;
-        JSONObject requestBodyPersona = new JSONObject();
-        try {
-            requestBodyPersona.put("nombre", nom1);
-            requestBodyPersona.put("nombre2", nom2);
-            requestBodyPersona.put("apellido", ape1);
-            requestBodyPersona.put("apellido2", ape2);
-            requestBodyPersona.put("telefono", tel);
-        } catch (JSONException j) {
-            j.printStackTrace();
-        }
-
-        JsonObjectRequest jsonObjectRequestPersona = new JsonObjectRequest(Request.Method.PUT, url, requestBodyPersona,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Toast.makeText(getApplicationContext(), "ACTUALIZACIÓN CORRECTA", Toast.LENGTH_LONG).show();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        }
-        );
-        Volley.newRequestQueue(this).add(jsonObjectRequestPersona);
+    private void manejarVolleyError(VolleyError error) {
+        Log.e("VolleyError", "Error en la solicitud", error);
+        mostrarMensaje("Error en la solicitud: " + error.getMessage());
     }
 
-    // Método para salir de la actividad
-    public void salir(View view) {
-        Intent inicio = new Intent(this, PantallaPrincipal.class);
-        startActivity(inicio);
+    private void mostrarMensaje(String mensaje) {
+        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
+    }
+
+    private void salir() {
+        Intent intent = new Intent(this, PantallaPrincipal.class);
+        startActivity(intent);
         finish();
     }
 }
--- studentstayo/app/src/main/java/com/tefa/studentstayo/model/Persona.java
+++ studentstayo/app/src/main/java/com/tefa/studentstayo/model/Persona.java
@@ -1,6 +1,7 @@
 package com.tefa.studentstayo.model;
 
 public class Persona {
+
     private String cedula_persona;
     private String nombre;
     private String nombre2;
@@ -9,10 +10,23 @@ public class Persona {
     private String telefono;
     private String direccion;
     private int edad;
+    private String genero;
+    private String id_canton;
+    // Nuevos atributos para contacto de emergencia
+    private String nombreContactoEmergencia;
+    private String telefonoContactoEmergencia;
+    private String parentescoContactoEmergencia;
+
+    // Nuevos atributos para contacto de emergencia #2
+    private String nombreContactoEmergencia2;
+    private String telefonoContactoEmergencia2;
+    private String parentescoContactoEmergencia2;
+
     public Persona() {
 
     }
-    public Persona(String cedula_persona, String nombre, String nombre2, String apellido, String apellido2, String telefono, String direccion, int edad) {
+
+    public Persona(String cedula_persona, String nombre, String nombre2, String apellido, String apellido2, String telefono, String direccion, int edad, String genero, String id_canton, String nombreContactoEmergencia, String telefonoContactoEmergencia, String parentescoContactoEmergencia, String nombreContactoEmergencia2, String telefonoContactoEmergencia2, String parentescoContactoEmergencia2) {
         this.cedula_persona = cedula_persona;
         this.nombre = nombre;
         this.nombre2 = nombre2;
@@ -21,53 +35,141 @@ public class Persona {
         this.telefono = telefono;
         this.direccion = direccion;
         this.edad = edad;
+        this.genero = genero;
+        this.id_canton = id_canton;
+        this.nombreContactoEmergencia = nombreContactoEmergencia;
+        this.telefonoContactoEmergencia = telefonoContactoEmergencia;
+        this.parentescoContactoEmergencia = parentescoContactoEmergencia;
+        this.nombreContactoEmergencia2 = nombreContactoEmergencia2;
+        this.telefonoContactoEmergencia2 = telefonoContactoEmergencia2;
+        this.parentescoContactoEmergencia2 = parentescoContactoEmergencia2;
     }
+
     public String getCedula_persona() {
         return cedula_persona;
     }
+
     public void setCedula_persona(String cedula_persona) {
         this.cedula_persona = cedula_persona;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getNombre2() {
         return nombre2;
     }
+
     public void setNombre2(String nombre2) {
         this.nombre2 = nombre2;
     }
+
     public String getApellido() {
         return apellido;
     }
+
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+
     public String getApellido2() {
         return apellido2;
     }
+
     public void setApellido2(String apellido2) {
         this.apellido2 = apellido2;
     }
+
     public String getTelefono() {
         return telefono;
     }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+
     public String getDireccion() {
         return direccion;
     }
+
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
+
     public int getEdad() {
         return edad;
     }
+
     public void setEdad(int edad) {
         this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getId_canton() {
+        return id_canton;
+    }
+
+    public void setId_canton(String id_canton) {
+        this.id_canton = id_canton;
+    }
+
+    public String getNombreContactoEmergencia() {
+        return nombreContactoEmergencia;
+    }
+
+    public void setNombreContactoEmergencia(String nombreContactoEmergencia) {
+        this.nombreContactoEmergencia = nombreContactoEmergencia;
+    }
+
+    public String getTelefonoContactoEmergencia() {
+        return telefonoContactoEmergencia;
+    }
+
+    public void setTelefonoContactoEmergencia(String telefonoContactoEmergencia) {
+        this.telefonoContactoEmergencia = telefonoContactoEmergencia;
+    }
+
+    public String getParentescoContactoEmergencia() {
+        return parentescoContactoEmergencia;
+    }
+
+    public void setParentescoContactoEmergencia(String parentescoContactoEmergencia) {
+        this.parentescoContactoEmergencia = parentescoContactoEmergencia;
+    }
+
+    public String getNombreContactoEmergencia2() {
+        return nombreContactoEmergencia2;
+    }
+
+    public void setNombreContactoEmergencia2(String nombreContactoEmergencia2) {
+        this.nombreContactoEmergencia2 = nombreContactoEmergencia2;
+    }
+
+    public String getTelefonoContactoEmergencia2() {
+        return telefonoContactoEmergencia2;
+    }
+
+    public void setTelefonoContactoEmergencia2(String telefonoContactoEmergencia2) {
+        this.telefonoContactoEmergencia2 = telefonoContactoEmergencia2;
+    }
+
+    public String getParentescoContactoEmergencia2() {
+        return parentescoContactoEmergencia2;
+    }
+
+    public void setParentescoContactoEmergencia2(String parentescoContactoEmergencia2) {
+        this.parentescoContactoEmergencia2 = parentescoContactoEmergencia2;
     }
 }
